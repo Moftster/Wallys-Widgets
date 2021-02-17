2d274ffe3ebc0dfbaa76abe8c79a586641d00ef2
DavidMoftakhar <david@chillaweb.com>
2021-02-17 18:40:37 +0000

    Include bootstrap, create page layout and landing page.

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('content')
+
+<div class="jumbotron">
+    <h1 class="display-4">Welcome to Wally's Widgets Calculator!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>Click below to start calculating how many packs of widgets will be required for your order!</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Calculate Widgets!</a>
+    </p>
+</div>
+
+@endsection
+
